3e65e50b03dbb9016b53eae48422a375c8fb9f52
Lenovo <2735749668@qq.com>
2019-05-16 21:31:30 +0800

    第一次提交

---
+++ app/src/main/java/com/example/tzontlilic/wonderful316/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.tzontlilic.wonderful316;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private TextView mTextView;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextView.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextView.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextView.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mTextView = (TextView) findViewById(R.id.textView);
+        mTextView.setText("");
+    }
+
+}
